b6aa3027f9c96532af1b1dac40017369aa878bbe
Ammu2604 <115615387+Ammu2604@users.noreply.github.com>
2023-03-31 20:21:23 +0530

    Initial commit

---
+++ abc.js
@@ -0,0 +1,55 @@
+// let person={
+//     name:'Praicy',
+//     age:23,
+//     job:'Engineer'
+// }
+// console.log(person.age)
+// var name="Praicy";
+// let age=23;
+// console.log(name);
+// console.log("Hello world");
+
+// //print numbers from 0-10
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+// //print numbers from 10-0
+// for(let i=10;i>=0;i--){
+//     console.log(i);
+// }
+// //print sum of 1st 10 natural number
+// let sum=0;
+// for(let i=1;i<=10;i++){
+//     sum = sum + i;
+// }
+// console.log('sum of 1st 10 natural numbers is', sum);
+
+
+
+//while loop
+
+// let i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//do-while
+
+// let i=2;
+// do{
+//     console.log(i);
+// }
+// while(i>2)
+
+
+
+let students = ['kanika','minnu','anakha']
+for(let student of students){
+    console.log(`Hello ${student}`)
+}
+
+let cars =['santro','i20','creta']
+for(let car of cars){
+    console.log(`hey ${car}`)
+}
---
+++ basics1.js
@@ -0,0 +1,24 @@
+a=5;
+var a=8;
+// console.log(a);
+{
+    var a=10;
+    //console.log(a);
+}
+// var s=a+10;
+// console.log(s);
+
+//.....Let is a block scope variable
+let b=10;
+//console.log(b);
+{
+    let b=8;
+    //console.log(b);
+}
+const pi=3.14;
+console.log(pi)
+{
+    const pi=10;
+    console.log(pi);
+}
+console.log(pi)
